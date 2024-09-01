512191fed0da4066f10069136b7b86a91d79667a
Вова <v.grishchenko@onpoint.ru>
2024-09-01 18:56:30 +0300

    hotfix

--- src/modules/Editor/hooks.ts
+++ src/modules/Editor/hooks.ts
@@ -1,9 +1,9 @@
 import EditorJS from "@editorjs/editorjs";
-import { ReactElement, RefObject, useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useState } from "react";
 import { config } from "./config";
 
 interface EditorConfig {
-  instanceRef: RefObject<ReactElement>;
+  instanceRef: MutableRefObject<(() => void) | null>;
 }
 export const InitEditor = ({ instanceRef }: EditorConfig) => {
   const [editor, setEditor] = useState<EditorJS>();
@@ -12,6 +12,9 @@ export const InitEditor = ({ instanceRef }: EditorConfig) => {
       if (instanceRef.current === null) {
         setEditor(new EditorJS(config));
       }
+      return () => {
+        instanceRef.current = null;
+      };
     }
   }, [instanceRef]);
 
